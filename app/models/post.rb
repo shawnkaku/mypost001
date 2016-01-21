805dc753f717ed74def83afa22e10df21fcde80d
shawnkaku <shawnkuo67@gmail.com>
2016-01-21 10:53:45 +0800

    category

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user, foreign_key: :user_id
-  has_many :comment, foreign_key: :post_id
+  has_many :comments, foreign_key: :post_id
   has_many :categories, through: :post_categories, foreign_key: :category_id, :class_name => 'Category'
   has_many :post_categories, foreign_key: :post_id
 end
