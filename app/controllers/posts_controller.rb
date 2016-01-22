3918f720d396c996c945207ee5be492892cb6b9f
shawnkaku <shawnkuo67@gmail.com>
2016-01-22 16:00:30 +0800

    add new post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,11 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  def new
+    @post = Post.new
+  end
+  def create
+    #binding pry
+
+  end
 end
